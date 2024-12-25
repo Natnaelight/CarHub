44a574d498f579b606b5d9ea8398c44dd78050d1
Light <natnaelkkidusan@gmail.com>
2024-12-25 18:52:26 +0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from 'react';
+import { CustomFilter, SearchBar, ShowMore } from '@/components';
+import Hero from '@/components/Hero';
+import { fetchCars } from '@/utils';
+import { CarCard } from '@/components';
+import { yearsOfProduction } from '@/constants';
+import { fuels } from '@/constants';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [allcars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [manufacturer, setManufacturer] = useState('');
+  const [model, setModel] = useState('');
+  const [year, setYear] = useState(2022);
+  const [fuel, setFuel] = useState('');
+  const [limit, setLimit] = useState(10);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const getCars = async () => {
+      setLoading(true);
+      try {
+        const result = await fetchCars({
+          manufacturer: manufacturer || '',
+          model: model || '',
+          year: year || 2022,
+          fuel: fuel || '',
+          limit: limit || 10,
+        });
+        setAllCars(result);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getCars();
+  }, [fuel, year, manufacturer, model, limit]);
+  const isDataEmpty = !Array.isArray(allcars) || allcars.length < 1 || !allcars;
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="home__filters">
+          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} setFilter={setFuel} />
+            <CustomFilter
+              title="year"
+              options={yearsOfProduction}
+              setFilter={setYear}
+            />
+          </div>
+        </div>
+        {allcars.length > 0 ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allcars?.map((car) => (
+                <CarCard car={car} key={car.city_mpg} />
+              ))}
+            </div>
+            {loading && (
+              <div className="mt-16 w-full flex-center">
+                <Image src="/loader.svg" alt="loader" width={50} height={50} />
+              </div>
+            )}
+            <ShowMore
+              pageNumber={limit / 10}
+              isNext={limit > allcars.length}
+              setLimit={setLimit}
+            />
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allcars?.message}</p>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
---
+++ components/CustomFilter.tsx
@@ -0,0 +1,75 @@
+'use client';
+import { updateSearchParams } from '@/utils';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Fragment } from 'react';
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+  Transition,
+} from '@headlessui/react';
+import { CustomFilterProps, OptionProps } from '@/Types';
+import Image from 'next/image';
+function CustomFilter({ title, options, setFilter }: CustomFilterProps) {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          setFilter(e.value);
+        }}
+      >
+        <div className="relative w-fit z-10">
+          <ListboxButton className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              alt="chevron up down"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+            />
+          </ListboxButton>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <ListboxOptions className="custom-filter__options">
+              {options.map((option) => (
+                <ListboxOption
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                    }`
+                  }
+                >
+                  {({ selected, active }) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? 'font-medium' : 'font-normal'
+                      } ${active ? 'text-white' : 'text-black'}`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
+
+export default CustomFilter;
